122c2552b10075bdb828b3513a3d08b85dd92f89
Devin Khun <lightningowl2004@gmail.com>
2024-09-25 20:40:18 -0700

    feat: express server and routing

---
+++ server/data/players.js
@@ -0,0 +1,104 @@
+const playerData = [
+  {
+    "id": 1,
+    "name": "Christian McCaffrey",
+    "team": "San Francisco 49ers",
+    "position": "Running Back",
+    "image": "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3117251.png&w=350&h=254",
+    "description": "",
+    "submittedBy": "Sasha",
+    "submittedOn": "2022-09-04T14:48:00"
+  },
+  {
+    "id": 2,
+    "name": "Amon-Ra St. Brown",
+    "team": "Detroit Lions",
+    "position": "Wide Receiver",
+    "image": "https://www.si.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cq_auto:good%2Cw_1200/MjAzNzY5MjY5Mjg3MTM0Nzgw/amon-ra-st-brown-tunnel.png",
+    "description": "Reliable target with exceptional route-running skills.",
+    "submittedBy": "Isabel",
+    "submittedOn": "2023-02-14T12:32:00"
+  },
+  {
+    "id": 3,
+    "name": "Travis Etienne",
+    "team": "Jacksonville Jaguars",
+    "position": "Running Back",
+    "image": "https://images2.minutemediacdn.com/image/upload/c_fill,w_1200,ar_1:1,f_auto,q_auto,g_auto/shape/cover/sport/https-3A-2F-2Fblackandteal-com-2Fwp-content-2Fuploads-2Fimagn-images-2F2017-2F07-2F18861006-850x560-fcc6d148c0ab2f979b76f466721b3ddbc2401caff24bbd0beaafcb1cafa8b862.jpg",
+    "description": "Explosive runner with great speed and pass-catching ability.",
+    "submittedBy": "Mario",
+    "submittedOn": "2022-09-13T11:01:00"
+  },
+  {
+    "id": 4,
+    "name": "Baker Mayfield",
+    "team": "Tampa Bay Buccaneers",
+    "position": "Quarterback",
+    "image": "https://atozsports.com/.image/t_share/MjA5NTQyMzcwNTE2MDE4MzQ1/usatsi_24300630.jpg",
+    "description": "A competitive quarterback known for his leadership and grit.",
+    "submittedBy": "Jason",
+    "submittedOn": "2023-03-10T05:23:00"
+  },
+  {
+    "id": 5,
+    "name": "Devon Achane",
+    "team": "Miami Dolphins",
+    "position": "Running Back",
+    "image": "https://static.clubs.nfl.com/image/private/t_person_squared_mobile/f_auto/dolphins/sktkvjzacrvdrngcpetw.jpg",
+    "description": "Fast and elusive with a great ability to make defenders miss.",
+    "submittedBy": "Pixie",
+    "submittedOn": "2011-10-10T14:48:00"
+  },
+  {
+    "id": 6,
+    "name": "Jayden Reed",
+    "team": "Green Bay Packers",
+    "position": "Wide Receiver",
+    "image": "https://packerswire.usatoday.com/wp-content/uploads/sites/57/2023/11/USATSI_21769715-e1701378813275.jpg?w=1000&h=600&crop=1",
+    "description": "Young and dynamic with high potential for big plays.",
+    "submittedBy": "Melody",
+    "submittedOn": "2011-10-10T14:48:00"
+  },
+  {
+    "id": 7,
+    "name": "Rashid Shaheed",
+    "team": "New Orleans Saints",
+    "position": "Wide Receiver",
+    "image": "https://dknetwork.draftkings.com/wp-content/uploads/USATSI_24242383-1.jpg",
+    "description": "Speedster with a knack for stretching defenses deep.",
+    "submittedBy": "Grover",
+    "submittedOn": "2023-01-01T14:48:00"
+  },
+  {
+    "id": 8,
+    "name": "David Montgomery",
+    "team": "Detroit Lions",
+    "position": "Running Back",
+    "image": "https://lionswire.usatoday.com/wp-content/uploads/sites/52/2024/01/22199951.jpg?w=1000&h=600&crop=1",
+    "description": "Powerful between-the-tackles runner with great vision.",
+    "submittedBy": "Marvin",
+    "submittedOn": "2023-04-01T07:02:00"
+  },
+  {
+    "id": 9,
+    "name": "Josh Allen",
+    "team": "Buffalo Bills",
+    "position": "Quarterback",
+    "image": "https://cdn.vox-cdn.com/thumbor/VwCqausEf4cyxGSbE3lgjNCDpII=/0x0:7334x4889/1200x800/filters:focal(3197x171:4369x1343)/cdn.vox-cdn.com/uploads/chorus_image/image/73606650/usa_today_24309199.0.jpg",
+    "description": "Dual-threat quarterback known for his strong arm and mobility.",
+    "submittedBy": "Brandon",
+    "submittedOn": "2023-01-10T14:48:00"
+  },
+  {
+    "id": 10,
+    "name": "Justin Jefferson",
+    "team": "Minnesota Vikings",
+    "position": "Wide Receiver",
+    "image": "https://a.espncdn.com/combiner/i?img=%2Fphoto%2F2024%2F0221%2Fr1294105_1296x729_16%2D9.jpg",
+    "description": "Elite wide receiver with incredible hands and route running.",
+    "submittedBy": "Michael",
+    "submittedOn": "2024-09-25T11:48:00"
+  }
+];
+
+export default playerData;
